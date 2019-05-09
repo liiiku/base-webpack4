4e255db8d1bb2dc03d06534a2cb5d56468d301a7
ruinan.li02 <ruinan.li02@ele.me>
2019-05-09 15:27:58 +0800

    feat:打包后的文件是否被压缩的mode配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/index.js',
+  // entry: './src/index.js',
+  mode: 'development', // 默认是production 不配置会有一个警告
+  entry: {
+    app: './src/index.js'
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
