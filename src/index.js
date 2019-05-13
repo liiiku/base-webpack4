f04b521bb418494ccefb9f681338238ab0a4c273
ruinan.li02 <ruinan.li02@ele.me>
2019-05-13 11:32:17 +0800

    feat:css-module模块样式单独作用，不影响其他文件

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-import sayTest from './test'
+// var jpg = require('./test.jpg')
+import jpgPath from './test.jpg'
+// import './index.css'
+// import './index.scss' // 这样引入样式实际上是全局的，也就是也影响了createAvatar 这个函数中的样式
+import style from './index.scss' // 会让这个模块里面的样式不会和其他的文件有什么的耦合和冲突
+import createAvatar from './createAvatar'
 
-console.log('hello world')
-sayTest()
+createAvatar() // 这里面的样式不受下面的style.jpg的影响，如果想要这里面的图片也有下面的jpg的样式，所以需要在这里面也加上className
+
+var img = new Image()
+img.src = jpgPath
+// img.classList.add('jpg')
+img.classList.add(style.jpg)
+
+var root = document.getElementById('root')
+root.append(img)
--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,40 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: { // 也就是打包模块的时候，不知道怎么办的时候，就到这里面来找了
+    rules: [
+      {
+        test: /\.(jpg|png|gif)$/,
+        use: {
+          loader: 'url-loader', // 和file-loader不同，会转成base64的字符串直接打包到bundle.js中
+          options: {
+            name: '[name].[hash:5].[ext]',
+            outputPath: 'images/',  // 打包到dist目录下的images目录中
+            limit: 2048 // 这里就是到底是用file-loader还是url-loader来打包，也就是说如果超过了2048个字节的话，就会想file-loader一样打包到一个文件夹中，如果没有，就url，打包到js中
+          }
+        }
+      },
+      // {
+      //   test: /\.css$/,
+      //   use: ['style-loader', 'css-loader'] // 因为需要用的loader不是一个了，所以这里就不能用一个对象了
+      // }
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', 
+          // 'css-loader', // 要个css-loader 加一些配置项，这里就不要写字符串了，要写对象
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2, // 在scss中通过import引入的scss文件，如果没有这个的话，就直接走css-loader style-loader 了，就不走postcss-loader 和 scss-loader，也就是不管是从里引入的，都是走所有的loader
+              modules: true // 开启css的模块化打包 这就是css-module的概念
+            }
+          },
+          'sass-loader',
+          'postcss-loader' // 默认加上厂商前缀 -webkit- -moz- postcss-loader 处理的时候，会找到postcss.config.js处理
+        ] // 因为需要用的loader不是一个了，所以这里就不能用一个对象了 先经过sass-loader 处理，再css-loader 再style-loader处理
+      }
+    ]
   }
 }
